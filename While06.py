fbb6094cb4be50ed44e7135c6a8ed3bbb3a1e9a9
unknown <afinis13s@gmail.com>
2024-01-16 08:38:09 +0500

    bajarilfi

--- While06.py
+++ While06.py
@@ -7,3 +7,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    s=0
+    a=0
+
+    while a<len(s):
+        if s[a].count("a","e","i","o","u"):
+            s+=1
+            a+=1
+    return s
